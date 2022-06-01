e3e16250ead1c82d4ff4fb7ba28377ca62326131
Justin Reeves <reeves122@gmail.com>
2022-06-01 16:48:17 -0500

    create example test

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,3 +1,41 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+locals {
+  git = "terraform-aws-redirect"
+}
+
+data "aws_route53_zone" "this" {
+  name = "oss.champtest.net."
+}
+
+module "vpc" {
+  source                   = "github.com/champ-oss/terraform-aws-vpc.git?ref=v1.0.1-afc8890"
+  git                      = local.git
+  availability_zones_count = 2
+  retention_in_days        = 1
+  create_private_subnets   = false
+}
+
+module "acm" {
+  source            = "github.com/champ-oss/terraform-aws-acm.git?ref=v1.0.19-aae8917"
+  git               = local.git
+  domain_name       = "${local.git}.${data.aws_route53_zone.this.name}"
+  create_wildcard   = false
+  zone_id           = data.aws_route53_zone.this.zone_id
+  enable_validation = true
+}
+
 module "this" {
-  source = "../../"
+  source            = "../../"
+  certificate_arn   = module.acm.arn
+  git               = local.git
+  route53_records   = ["${local.git}.${data.aws_route53_zone.this.name}"]
+  host              = "github.com"
+  path              = "/search"
+  query             = "q=aws"
+  zone_id           = data.aws_route53_zone.this.zone_id
+  public_subnet_ids = module.vpc.public_subnets_ids
+  vpc_id            = module.vpc.vpc_id
 }
