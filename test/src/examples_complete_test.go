8e32679a8298eed24dd97d1cf0ec7801afab1082
Justin Reeves <reeves122@gmail.com>
2022-06-01 16:47:54 -0500

    update terratest code

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -1,7 +1,10 @@
 package test
 
 import (
+	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+	"net/http"
 	"testing"
 )
 
@@ -17,4 +20,18 @@ func TestExamplesComplete(t *testing.T) {
 	}
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApplyAndIdempotent(t, terraformOptions)
+
+	logger.Log(t, "Testing HTTP and HTTPS redirects")
+	checkRedirect(t, "http://terraform-aws-redirect.oss.champtest.net/", "https://github.com:443/search?q=aws")
+	checkRedirect(t, "https://terraform-aws-redirect.oss.champtest.net/", "https://github.com:443/search?q=aws")
+}
+
+func checkRedirect(t *testing.T, url string, expected string) {
+	logger.Log(t, "Testing redirect for :", url)
+	resp, err := http.Get(url)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+
+	logger.Log(t, "Checking if redirect works with host, path, query for:", expected)
+	assert.Equal(t, expected, resp.Request.URL.String())
 }
